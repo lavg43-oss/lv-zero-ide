3c28b1f78fc726a88d9897385abaa3b0f70027e2
Luis Vallejo <lavg43@secundaria11jtb.com>
2026-06-29 19:25:48 -0600

    fix: Add 'unsafe-inline' to CSP script-src for inline scripts

--- src/main.cjs
+++ src/main.cjs
@@ -691,7 +691,7 @@ function setupCSP() {
             "Content-Security-Policy": [
               "default-src 'self'; " +
               "style-src 'self' 'unsafe-inline'; " +
-              "script-src 'self' 'unsafe-eval'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
               "img-src 'self' data:; " +
               "font-src 'self' data:; " +
               "connect-src 'self' http://localhost:* ws://localhost:* https:;"
